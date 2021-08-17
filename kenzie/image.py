1d7e79c8f310cb523d98af5b58701aa97e560cc0
Lucira Silva <luciracs0@gmail.com>
2021-08-17 11:32:13 -0300

    feat: add lib with functions

---
+++ kenzie/image.py
@@ -0,0 +1,49 @@
+from flask import jsonify, request, send_from_directory
+import os
+
+FILES_DIRECTORY = './files'
+SUPPORTED_FILES = ['jpg', 'png', 'gif']
+
+def get_files(file_type: str):
+    if file_type == 'all':
+        output_list = [dir_files for _, _, dir_files in os.walk(FILES_DIRECTORY) if len(dir_files) != 0]
+        return jsonify(output_list), 200
+    else:
+        if file_type in SUPPORTED_FILES:
+            output_list = os.listdir(f'{FILES_DIRECTORY}/{file_type}')
+            return jsonify(output_list), 200
+          
+
+def upload_image():
+    received_file = request.files['file']
+    file_name = received_file.filename
+    extension_file = file_name.split('.')[-1]
+
+    if len([extension for extension in SUPPORTED_FILES if extension_file == extension]) == 0:
+        return 'Formato inválido, Tente enviar arquivos jpg, gif ou png', 415
+    elif os.path.exists(f'{FILES_DIRECTORY}/{extension_file}/{file_name}'):
+        return {'message': 'Arquivo já existente'}, 409
+
+    try:
+        path = f'{FILES_DIRECTORY}/{extension_file}'
+        file_name = received_file.filename
+        received_file.save(f'{path}/{file_name}')
+    except (SyntaxError, TypeError, IsADirectoryError):
+        return {'message': """Erro ao adicionar arquivo.
+        Certifique-se de que adicionou a chave e o arquivo correto"""}, 406
+    except FileNotFoundError:
+        os.mkdir(path)
+        received_file.save(f'{path}/{file_name}')
+    return {'message': f'Arquivo {file_name} adicionado com sucesso!'}, 201
+
+
+def download_file(file_name: str):
+    extension_file = file_name.split('.')[-1]
+    return send_from_directory(directory=f"../files/{extension_file}", path=file_name, as_attachment=True)
+
+
+def download_zip_file():
+    file_type = request.args.get('file_type')
+    compression_rate = request.args.get('compression_rate')
+    os.system(f"zip -{compression_rate} -r /tmp/imagens.zip {FILES_DIRECTORY}/{file_type}")
+    return send_from_directory(directory="/tmp", path='imagens.zip', as_attachment=True)
