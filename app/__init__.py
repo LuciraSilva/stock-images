928fa11a961d427be0e58a8aae7deafd6ecd1002
Lucira Silva <luciracs0@gmail.com>
2021-08-18 11:35:19 -0300

    refact: add in init to create all necessary folders and remove except FileNotFound from image.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,14 @@ import os
 
 
 FILES_DIRECTORY = './files'
+PNG_DIRECTORY = f'{FILES_DIRECTORY}/png'
+JPG_DIRECTORY = f'{FILES_DIRECTORY}/jpg'
+GIF_DIRECTORY = f'{FILES_DIRECTORY}/gif'
+
 
 if not os.path.exists(FILES_DIRECTORY):
     os.mkdir(FILES_DIRECTORY)
+    os.mkdir(PNG_DIRECTORY)
+    os.mkdir(JPG_DIRECTORY)
+    os.mkdir(GIF_DIRECTORY)
+
--- kenzie/image.py
+++ kenzie/image.py
@@ -2,13 +2,13 @@ from flask import jsonify, request, send_from_directory
 import os
 from app import FILES_DIRECTORY
 
-SUPPORTED_FILES = ['jpg', 'png', 'gif']
+SUPPORTED_EXTENSIONS = ['jpg', 'png', 'gif']
 
 def get_files(file_type: str):
     if not file_type:
         output_list = [dir_files for _, _, dir_files in os.walk(FILES_DIRECTORY) if len(dir_files) != 0]
         return jsonify(output_list), 200
-    elif file_type in SUPPORTED_FILES:
+    elif file_type in SUPPORTED_EXTENSIONS:
         output_list = os.listdir(f'{FILES_DIRECTORY}/{file_type}')
         return jsonify(output_list), 200
     return {'message': 'Formato inválido'}, 415
@@ -18,7 +18,7 @@ def upload_image():
     file_name = received_file.filename
     extension_file = file_name.split('.')[-1]
 
-    if len([extension for extension in SUPPORTED_FILES if extension_file == extension]) == 0:
+    if len([extension for extension in SUPPORTED_EXTENSIONS if extension_file == extension]) == 0:
         return 'Formato inválido, Tente enviar arquivos jpg, gif ou png', 415
     elif os.path.exists(f'{FILES_DIRECTORY}/{extension_file}/{file_name}'):
         return {'message': 'Arquivo já existente'}, 409
@@ -30,9 +30,6 @@ def upload_image():
     except (SyntaxError, TypeError, IsADirectoryError):
         return {'message': """Erro ao adicionar arquivo.
         Certifique-se de que adicionou a chave e o arquivo correto"""}, 406
-    except FileNotFoundError:
-        os.mkdir(path)
-        received_file.save(f'{path}/{file_name}')
     return {'message': f'Arquivo {file_name} adicionado com sucesso!'}, 201
 
 
