7c5629f15675385404726794cba59f888878647a
Lucira Silva <luciracs0@gmail.com>
2021-09-17 18:50:28 -0300

    refact: change bp name in routes init

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 from . import images_blueprint
 
-bp = Blueprint('api_bp', __name__, url_prefix='/api')
+bp = Blueprint('bp_api', __name__, url_prefix='/api')
 
 bp.register_blueprint(images_blueprint.bp)
